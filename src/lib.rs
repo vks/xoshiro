9d77d1a057e3bc5323126218899d2de1cbd07a58
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2018-06-11 15:31:23 +0200

    Implement Xoshiro256**

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 extern crate rand_core;
 
 mod xoshiro128starstar;
+mod xoshiro256starstar;
 
 pub use xoshiro128starstar::Xoshiro128StarStar;
+pub use xoshiro256starstar::Xoshiro256StarStar;
---
+++ src/xoshiro256starstar.rs
@@ -0,0 +1,86 @@
+
+use rand_core::impls::fill_bytes_via_next;
+use rand_core::le::read_u64_into;
+use rand_core::{SeedableRng, RngCore, Error};
+
+fn rotl(x: u64, k: u64) -> u64 {
+    (x << k) | (x >> (64 - k))
+}
+
+fn starstar(s0: u64) -> u64 {
+    rotl(s0.wrapping_mul(5), 7).wrapping_mul(9)
+}
+
+#[derive(Debug, Clone)]
+pub struct Xoshiro256StarStar {
+    s: [u64; 4],
+}
+
+impl SeedableRng for Xoshiro256StarStar {
+    type Seed = [u8; 32];
+
+    #[inline]
+    fn from_seed(seed: [u8; 32]) -> Xoshiro256StarStar {
+        let mut state = [0; 4];
+        read_u64_into(&seed, &mut state);
+        Xoshiro256StarStar { s: state }
+    }
+}
+
+impl RngCore for Xoshiro256StarStar {
+    #[inline]
+    fn next_u32(&mut self) -> u32 {
+        self.next_u64() as u32
+    }
+
+    #[inline]
+    fn next_u64(&mut self) -> u64 {
+        let result_starstar = starstar(self.s[1]);
+
+        let t = self.s[1] << 17;
+
+        self.s[2] ^= self.s[0];
+        self.s[3] ^= self.s[1];
+        self.s[1] ^= self.s[2];
+        self.s[0] ^= self.s[3];
+
+        self.s[2] ^= t;
+
+        self.s[3] = rotl(self.s[3], 45);
+
+        result_starstar
+    }
+
+    #[inline]
+    fn fill_bytes(&mut self, dest: &mut [u8]) {
+        fill_bytes_via_next(self, dest);
+    }
+
+    #[inline]
+    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), Error> {
+        self.fill_bytes(dest);
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_xoshiro() {
+        let mut rng = Xoshiro256StarStar::from_seed(
+            [1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0,
+             3, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0]);
+        // These values were produced with the reference implementation:
+        // http://xoshiro.di.unimi.it/xoshiro128starstar.c
+        let expected = [
+            11520, 0, 1509978240, 1215971899390074240, 1216172134540287360,
+            607988272756665600, 16172922978634559625, 8476171486693032832,
+            10595114339597558777, 2904607092377533576,
+        ];
+        for &e in &expected {
+            assert_eq!(rng.next_u64(), e);
+        }
+    }
+}
