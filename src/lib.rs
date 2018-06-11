39e7bb27f669324099b9dfb86d7a888b03e6916e
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2018-06-11 16:10:01 +0200

    Implement Xoroshiro128Plus

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,9 @@ extern crate rand_core;
 mod splitmix64;
 mod xoshiro128starstar;
 mod xoshiro256starstar;
+mod xoroshiro128plus;
 
 pub use splitmix64::SplitMix64;
 pub use xoshiro128starstar::Xoshiro128StarStar;
 pub use xoshiro256starstar::Xoshiro256StarStar;
+pub use xoroshiro128plus::Xoroshiro128Plus;
