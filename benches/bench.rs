5352324f1a52ecbc31169bfaec3aa766d201af27
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2018-06-11 11:40:17 +0200

    Remove SIMD code
    Unfortunately it was slower than the non-SIMD code.

--- benches/bench.rs
+++ benches/bench.rs
@@ -24,14 +24,6 @@ macro_rules! make_bench_u32 {
 }
 
 make_bench_u32!(rand_u32_xoshiro, Xoshiro128);
-#[cfg(all(any(target_arch = "x86", target_arch = "x86_64"),
-      target_feature = "avx2"))]
-make_bench_u32!(rand_u32_xoshiro_simd, xoshiro::avx2::Xoshiro128);
 
-#[cfg(all(any(target_arch = "x86", target_arch = "x86_64"),
-      target_feature = "avx2"))]
-benchmark_group!(benches, rand_u32_xoshiro, rand_u32_xoshiro_simd);
-#[cfg(not(all(any(target_arch = "x86", target_arch = "x86_64"),
-      target_feature = "avx2")))]
 benchmark_group!(benches, rand_u32_xoshiro);
 benchmark_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,5 @@
-
 fn rotl(x: u32, k: u32) -> u32 {
     (x << k) | (x >> (32 - k))
-}
-
-
-fn to_i32(x: u32) -> i32 {
-    unsafe { std::mem::transmute(x) }
-}
-
-fn to_u32(x: i32) -> u32 {
-    unsafe { std::mem::transmute(x) }
 }
 
 fn starstar(s0: u32) -> u32 {
@@ -32,26 +22,6 @@ impl Xoshiro128 {
         let result_starstar = starstar(self.s[0]);
         let t = self.s[1] << 9;
 
-        /*
-        // This code is closer to the SIMD implementation.
-        // It is slightly inefficient, because reordering the assignments
-        // yields the same results with less instructions. However, this is
-        // closer to the SIMD algorithm and might be optimized away by the
-        // compiler.
-        fn shuffle(s: [u32; 4]) -> [u32; 4] { [s[3], s[2], s[0], s[1]] }
-
-        fn xor(a: [u32; 4], b: [u32; 4]) -> [u32; 4] {
-            [a[0] ^ b[0], a[1] ^ b[1], a[2] ^ b[2], a[3] ^ b[3]]
-        }
-
-        let s0 = self.s[0];
-        let s1 = self.s[1];
-        self.s = xor(self.s, shuffle(self.s));
-        self.s[0] ^= s1;
-        self.s[1] ^= s0;
-        */
-
-        // This is closer to the reference implementation.
         self.s[2] ^= self.s[0];
         self.s[3] ^= self.s[1];
         self.s[1] ^= self.s[2];
@@ -64,57 +34,6 @@ impl Xoshiro128 {
         result_starstar
     }
 }
-
-#[cfg(all(any(target_arch = "x86", target_arch = "x86_64"),
-      target_feature = "avx2"))]
-pub mod avx2 {
-    #[cfg(target_arch = "x86")]
-    use std::arch::x86::*;
-    #[cfg(target_arch = "x86_64")]
-    use std::arch::x86_64::*;
-    use super::{rotl, starstar, to_u32, to_i32};
-
-    #[derive(Debug, Clone)]
-    pub struct Xoshiro128 {
-        s: __m128i,
-    }
-
-    impl Xoshiro128 {
-        #[inline]
-        pub fn from_seed(s: [u32; 4]) -> Xoshiro128 {
-            unsafe {
-                Xoshiro128 {
-                    s: _mm_set_epi32(to_i32(s[0]), to_i32(s[1]),
-                                     to_i32(s[2]), to_i32(s[3]))
-                }
-            }
-        }
-
-        #[inline]
-        pub fn next(&mut self) -> u32 {
-            unsafe {
-                let s0 = _mm_extract_epi32(self.s, 3);
-                let s1 = _mm_extract_epi32(self.s, 2);
-                let result_starstar = starstar(to_u32(s0));
-                let shifted = to_u32(s1) << 9;
-                let t = _mm_set_epi32(0, 0, to_i32(shifted), 0);
-                let shuffled = _mm_shuffle_epi32(self.s, 0b00_01_11_10);
-                self.s = _mm_xor_si128(self.s, shuffled);
-                self.s = _mm_xor_si128(self.s, _mm_set_epi32(s1, s0, 0, 0));
-                self.s = _mm_xor_si128(self.s, t);
-                let s3 = to_u32(_mm_extract_epi32(self.s, 0));
-                let u = _mm_set_epi32(0, 0, 0, to_i32(rotl(s3, 11)));
-                 // `_mm_blend_epi32` requires avx2. In principle this could also
-                 // be implemented using `_mm_blend_epi16`, which requires only
-                 // sse4.1.
-                self.s = _mm_blend_epi32(self.s, u, 0b0001);
-
-                result_starstar
-            }
-        }
-    }
-}
-
 
 #[cfg(test)]
 mod tests {
@@ -131,17 +50,6 @@ mod tests {
         ];
         for &e in &expected {
             assert_eq!(rng.next(), e);
-        }
-    }
-
-    #[test]
-    #[cfg(all(any(target_arch = "x86", target_arch = "x86_64"),
-          target_feature = "avx2"))]
-    fn test_xoshiro_simd() {
-        let mut rng1 = Xoshiro128::from_seed([1, 2, 3, 4]);
-        let mut rng2 = super::avx2::Xoshiro128::from_seed([1, 2, 3, 4]);
-        for _ in 0..100 {
-            assert_eq!(rng1.next(), rng2.next());
         }
     }
 }
